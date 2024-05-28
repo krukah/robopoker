9529b6d7f29f9ee4e73c41cc11e24876ce056f37
Kelechi Ukah <ukahkelechi@gmail.com>
2024-05-27 22:30:07 -0400

    deletions

--- src/cfr/training/mod.rs
+++ src/cfr/training/mod.rs
@@ -1,13 +1,6 @@
 pub(crate) type Utility = f32;
 pub(crate) type Probability = f32;
 
-pub(crate) mod action;
-pub(crate) mod info;
-pub(crate) mod node;
-pub(crate) mod optimizer;
-pub(crate) mod player;
-pub(crate) mod policy;
-pub(crate) mod profile;
-pub(crate) mod strategy;
-pub(crate) mod trainer;
+pub(crate) mod learning;
+pub(crate) mod marker;
 pub(crate) mod tree;
--- src/cfr/training/player.rs
+++
@@ -1,2 +0,0 @@
-/// An element of the finite set N of players, including chance.
-pub(crate) trait Player: Eq + Copy {}
