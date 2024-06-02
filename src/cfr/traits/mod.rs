57f8438c151ac2a10deb0a02b43a64324634fc53
Kelechi Ukah <ukahkelechi@gmail.com>
2024-06-02 15:04:53 -0400

    popping off in mod.rs

--- src/cfr/traits/mod.rs
+++ src/cfr/traits/mod.rs
@@ -1,7 +1,4 @@
 #![allow(dead_code)]
-
-use std::hash::Hash;
-
 pub(crate) mod marker;
 pub(crate) mod training;
 pub(crate) mod tree;
@@ -9,224 +6,313 @@ pub(crate) mod tree;
 pub(crate) type Utility = f32;
 pub(crate) type Probability = f32;
 
-trait Action: Eq + Sized + Hash {}
-trait Bucket: Eq + Sized + Hash {}
-trait Player: Eq + Sized {}
+use petgraph::graph::DiGraph;
+use petgraph::graph::NodeIndex;
+use petgraph::Direction::Incoming;
+use petgraph::Direction::Outgoing;
+use std::hash::Hash;
 
-// /// observable features of a unique history + game state, abstracted over the type of player and bucket
-// trait Visible<E, B, Y>
-// where
-//     Self: Sized,
-//     E: Action,
-//     B: Bucket,
-//     Y: Player,
-// {
-//     fn bucket(&self) -> &B;
-//     fn player(&self) -> &Y;
-//     fn available(&self) -> Vec<&E>;
-// }
+trait Player<'t>: 't + Sized + Eq {}
+trait Bucket<'t>: 't + Sized + Eq + Hash {}
+trait Action<'t>: 't + Sized + Eq + Hash + Copy {}
 
-// /// the thing that's able to self-replicate, defining how the tree builds itself
-// trait Growable<E>
-// where
-//     Self: Sized,
-//     E: Action,
-// {
-//     fn root() -> Self;
-//     fn spawn(&self) -> Vec<(Self, E)>;
-// }
+/// the inner state of a node, abstracted over the type of action and bucket
+trait Vertex<'t, A, B, C>
+where
+    Self: 't + Sized,
+    A: Action<'t>,
+    B: Bucket<'t>,
+    C: Player<'t>,
+{
+    // required
+    fn root() -> Self;
+    fn expand(&'t self) -> Vec<(Self, A)>;
+    fn bucket(&'t self) -> &'t B;
+    fn player(&'t self) -> &'t C;
+    fn payoff(&'t self, player: &'t C) -> Utility;
+}
 
-// /// collection of these three is what you would get in a Node, which may be too restrictive for a lot of the use so we'll se
-// trait Node<V, E, B, Y>
-// where
-//     Self: Visible<E, B, Y>,
-//     V: Growable<E>,
-//     E: Action,
-//     B: Bucket,
-//     Y: Player,
-// {
-//     // obserability
-//     fn utility(&self, player: &Y) -> Utility;
-//     // walkability
-//     fn parent(&self) -> Option<&Self>;
-//     fn children(&self) -> Vec<&Self>;
-//     fn incoming(&self) -> Option<&E>;
-//     fn outgoing(&self) -> Vec<&E>;
-//     fn descendants(&self) -> Vec<&Self> {
-//         match self.children().len() {
-//             0 => vec![&self],
-//             _ => self
-//                 .children()
-//                 .iter()
-//                 .map(|child| child.descendants())
-//                 .flatten()
-//                 .collect(),
-//         }
-//     }
-//     fn follow(&self, edge: &E) -> &Self {
-//         self.children()
-//             .iter()
-//             .find(|child| edge == child.incoming().unwrap())
-//             .unwrap()
-//     }
-// }
+/// collection of these three is what you would get in a Node, which may be too restrictive for a lot of the use so we'll se
+trait Node<'t, V, A, B, C>
+where
+    Self: 't + Sized,
+    V: Vertex<'t, A, B, C>,
+    A: Action<'t>,
+    B: Bucket<'t>,
+    C: Player<'t>,
+{
+    // required
+    fn local(&'t self) -> &'t V;
+    fn index(&'t self) -> &'t NodeIndex;
+    fn graph(&'t self) -> &'t DiGraph<Self, A>;
 
-// /// implementation using petgraph::graph::DiGraph
+    // obserability (implemented from Vertex)
+    fn payoff(&'t self, player: &'t C) -> Utility {
+        self.local().payoff(player)
+    }
+    fn bucket(&'t self) -> &'t B {
+        self.local().bucket()
+    }
+    fn player(&'t self) -> &'t C {
+        self.local().player()
+    }
 
-// /// distribution over indistinguishable nodes, abstracted over the type of node
-// trait Info<V, E, B, Y>
-// where
-//     Self: Visible<E, B, Y>,
-//     E: Action,
-//     B: Bucket,
-//     Y: Player,
-// {
-//     fn roots(&self) -> Vec<&Node<V, E, B, Y>>;
-// }
+    // walkability
+    fn parent(&'t self) -> Option<&'t Self> {
+        self.graph()
+            .neighbors_directed(*self.index(), Incoming)
+            .next()
+            .map(|index| {
+                self.graph()
+                    .node_weight(index)
+                    .expect("tree property: if incoming edge, then parent")
+            })
+    }
+    fn children(&'t self) -> Vec<&'t Self> {
+        self.graph()
+            .neighbors_directed(*self.index(), Outgoing)
+            .map(|c| {
+                self.graph()
+                    .node_weight(c)
+                    .expect("tree property: if outgoing edge, then child")
+            })
+            .collect()
+    }
+    fn incoming(&'t self) -> Option<&'t A> {
+        self.graph()
+            .edges_directed(*self.index(), Incoming)
+            .next()
+            .map(|e| e.weight())
+    }
+    fn outgoing(&'t self) -> Vec<&'t A> {
+        self.graph()
+            .edges_directed(*self.index(), Outgoing)
+            .map(|e| e.weight())
+            .collect()
+    }
+    fn descendants(&'t self) -> Vec<&'t Self> {
+        match self.children().len() {
+            0 => vec![&self],
+            _ => self
+                .children()
+                .iter()
+                .map(|child| child.descendants())
+                .flatten()
+                .collect(),
+        }
+    }
+    fn follow(&'t self, edge: &'t A) -> &'t Self {
+        self.children()
+            .iter()
+            .find(|child| edge == child.incoming().unwrap())
+            .unwrap()
+    }
+}
 
-// /// a policy π is a distribution over actions given a node. Equivalently a vector indexed by action ∈ A
-// trait Distribution<E>
-// where
-//     E: Action,
-// {
-//     fn weight(&self, action: &E) -> Probability;
-//     fn sample(&self) -> &E;
-// }
+/// distribution over indistinguishable nodes, abstracted over the type of node
+trait Info<'t, N, V, A, B, C>
+where
+    N: Node<'t, V, A, B, C>,
+    V: Vertex<'t, A, B, C>,
+    A: Action<'t>,
+    B: Bucket<'t>,
+    C: Player<'t>,
+{
+    // required
+    fn roots(&'t self) -> &'t Vec<&'t N>;
 
-// /// a strategy σ is a policy for each player. Equivalently a matrix indexed by (player, action) or (i,a) ∈ N × A
-// trait Strategy<D, E, B>
-// where
-//     D: Distribution<E>,
-//     E: Action,
-//     B: Bucket,
-// {
-//     fn policy(&self, bucket: &B) -> &D;
-// }
+    fn bucket(&'t self) -> &'t B {
+        self.roots().iter().next().unwrap().bucket()
+    }
+    fn player(&'t self) -> &'t C {
+        self.roots().iter().next().unwrap().player()
+    }
+    fn outgoing(&'t self) -> Vec<&'t A> {
+        self.roots().iter().next().unwrap().outgoing()
+    }
+}
 
-// /// a profile σ consists of a strategy for each player. Equivalently a matrix indexed by (player, action) or (i,a) ∈ N × A
-// trait Profile<S, D, N, E, B, Y>
-// where
-//     S: Strategy<D, E, B>,
-//     N: Node<E, B, Y>,
-//     D: Distribution<E>,
-//     E: Action,
-//     B: Bucket,
-//     Y: Player,
-// {
-//     fn strategy(&self, player: &Y) -> &S;
-//     // provided
-//     fn gain(&self, root: &N, action: &E) -> Utility {
-//         self.cfactual_value(root, action) - self.expected_value(root)
-//     }
-//     fn cfactual_value(&self, root: &N, action: &E) -> Utility {
-//         self.cfactual_reach(root)
-//             * root //                                       suppose you're here on purpose, counterfactually
-//                 .follow(action) //                          suppose you're here on purpose, counterfactually
-//                 .descendants() //                           O(depth) recursive downtree
-//                 .iter() //                                  duplicated calculation
-//                 .map(|leaf| self.relative_value(root, leaf))
-//                 .sum::<Utility>()
-//     }
-//     fn expected_value(&self, root: &N) -> Utility {
-//         self.expected_reach(root)
-//             * root
-//                 .descendants() //                           O(depth) recursive downtree
-//                 .iter() //                                  duplicated calculation
-//                 .map(|leaf| self.relative_value(root, leaf))
-//                 .sum::<Utility>()
-//     }
-//     fn relative_value(&self, root: &N, leaf: &N) -> Utility {
-//         leaf.utility(root.player())
-//             * self.relative_reach(root, leaf)
-//             * self.sampling_reach(root, leaf)
-//     }
-//     // probability calculations
-//     fn weight(&self, node: &N, action: &E) -> Probability {
-//         self.strategy(node.player())
-//             .policy(node.bucket())
-//             .weight(action)
-//     }
-//     fn cfactual_reach(&self, node: &N) -> Probability {
-//         match node.parent() {
-//             None => 1.0,
-//             Some(parent) => {
-//                 self.cfactual_reach(parent)
-//                     * if node.player() == parent.player() {
-//                         1.0
-//                     } else {
-//                         self.weight(
-//                             parent,
-//                             node.incoming().expect("if has parent, then has incoming"),
-//                         )
-//                     }
-//             }
-//         }
-//     }
-//     fn expected_reach(&self, node: &N) -> Probability {
-//         match node.parent() {
-//             None => 1.0,
-//             Some(parent) => {
-//                 self.expected_reach(parent)
-//                     * self.weight(
-//                         parent,
-//                         node.incoming().expect("if has parent, then has incoming"),
-//                     )
-//             }
-//         }
-//     }
-//     fn relative_reach(&self, root: &N, leaf: &N) -> Probability {
-//         //? gotta optimize out integration over shared ancestors that cancels out in this division. Node: Eq? Hash?
-//         self.expected_reach(leaf) / self.expected_reach(root)
-//     }
-//     fn sampling_reach(&self, _: &N, _: &N) -> Probability {
-//         1.0
-//     }
-// }
+/// a tree will own the graph and infosets
+trait Tree<'t, I, N, V, A, B, C>
+where
+    I: Info<'t, N, V, A, B, C>,
+    N: Node<'t, V, A, B, C>,
+    V: Vertex<'t, A, B, C>,
+    A: Action<'t>,
+    B: Bucket<'t>,
+    C: Player<'t>,
+{
+    // required
+    fn infosets(&'t self) -> &'t Vec<&'t I>;
+}
 
-// /// trainer will update regrets and profile in a mutable loop
-// trait Trainer<P, S, D, N, E, B, I, Y>
-// where
-//     P: Profile<S, D, N, E, B, Y>,
-//     S: Strategy<D, E, B>,
-//     I: Info<N, E, B, Y>,
-//     N: Node<E, B, Y>,
-//     D: Distribution<E>,
-//     E: Action,
-//     B: Bucket,
-//     Y: Player,
-// {
-//     fn profile(&self) -> &P;
+/// a policy π is a distribution over actions given a bucket. Equivalently a vector indexed by action ∈ A
+trait Distribution<'t, A>
+where
+    A: Action<'t>,
+{
+    // required
+    fn weight(&self, action: &A) -> Probability;
+    fn sample(&self) -> &A;
+}
 
-//     fn update_regret(&mut self, info: &I);
-//     fn update_policy(&mut self, info: &I);
+/// a strategy σ is a policy for each player. Equivalently a matrix indexed by (player, action) or (i,a) ∈ N × A
+trait Strategy<'t, D, A, B>
+where
+    D: Distribution<'t, A>,
+    A: Action<'t>,
+    B: Bucket<'t>,
+{
+    // required
+    fn policy(&self, bucket: &B) -> &D;
+}
 
-//     fn current_regret(&self, info: &I, action: &E) -> Utility;
-//     fn instant_regret(&self, info: &I, action: &E) -> Utility {
-//         info.roots()
-//             .iter()
-//             .map(|root| self.profile().gain(root, action))
-//             .sum::<Utility>()
-//     }
-//     fn pending_regret(&self, info: &I, action: &E) -> Utility {
-//         self.instant_regret(info, action) + self.current_regret(info, action)
-//     }
+/// a profile σ consists of a strategy for each player. Equivalently a matrix indexed by (player, action) or (i,a) ∈ N × A
+trait Profile<'t, S, D, N, V, A, B, C>
+where
+    S: Strategy<'t, D, A, B>,
+    D: Distribution<'t, A>,
+    N: Node<'t, V, A, B, C>,
+    V: Vertex<'t, A, B, C>,
+    A: Action<'t>,
+    B: Bucket<'t>,
+    C: Player<'t>,
+{
+    // required
+    fn strategy(&self, player: &C) -> &S;
 
-//     fn policy_vector(&self, info: &I) -> Vec<(E, Probability)> {
-//         let regrets = info
-//             .available()
-//             .iter()
-//             .map(|action| (**action, self.current_regret(info, action)))
-//             .map(|(a, r)| (a, r.max(Utility::MIN_POSITIVE)))
-//             .collect::<Vec<(E, Probability)>>();
-//         let sum = regrets.iter().map(|(_, r)| r).sum::<Utility>();
-//         let policy = regrets.into_iter().map(|(a, r)| (a, r / sum)).collect();
-//         policy
-//         // uses RegretMatching+ to compute policy from current regrets
-//     }
-//     fn regret_vector(&self, info: &I) -> Vec<(E, Utility)> {
-//         info.available()
-//             .iter()
-//             .map(|action| (**action, self.pending_regret(info, action)))
-//             .collect()
-//     }
-// }
+    // provided
+    fn gain(&self, root: &'t N, action: &'t A) -> Utility {
+        self.cfactual_value(root, action) - self.expected_value(root)
+    }
+    fn cfactual_value(&self, root: &'t N, action: &'t A) -> Utility {
+        self.cfactual_reach(root)
+            * root //                                       suppose you're here on purpose, counterfactually
+                .follow(action) //                          suppose you're here on purpose, counterfactually
+                .descendants() //                           O(depth) recursive downtree
+                .iter() //                                  duplicated calculation
+                .map(|leaf| self.relative_value(root, leaf))
+                .sum::<Utility>()
+    }
+    fn expected_value(&self, root: &'t N) -> Utility {
+        self.expected_reach(root)
+            * root
+                .descendants() //                           O(depth) recursive downtree
+                .iter() //                                  duplicated calculation
+                .map(|leaf| self.relative_value(root, leaf))
+                .sum::<Utility>()
+    }
+    fn relative_value(&self, root: &'t N, leaf: &'t N) -> Utility {
+        leaf.payoff(root.player())
+            * self.relative_reach(root, leaf)
+            * self.sampling_reach(root, leaf)
+    }
+    // probability calculations
+    fn weight(&self, node: &'t N, action: &A) -> Probability {
+        self.strategy(node.player())
+            .policy(node.bucket())
+            .weight(action)
+    }
+    fn cfactual_reach(&self, node: &'t N) -> Probability {
+        match node.parent() {
+            None => 1.0,
+            Some(parent) => {
+                self.cfactual_reach(parent)
+                    * if node.player() == parent.player() {
+                        1.0
+                    } else {
+                        self.weight(
+                            parent,
+                            node.incoming().expect("if has parent, then has incoming"),
+                        )
+                    }
+            }
+        }
+    }
+    fn expected_reach(&self, node: &'t N) -> Probability {
+        match node.parent() {
+            None => 1.0,
+            Some(parent) => {
+                self.expected_reach(parent)
+                    * self.weight(
+                        parent,
+                        node.incoming().expect("if has parent, then has incoming"),
+                    )
+            }
+        }
+    }
+    fn relative_reach(&self, root: &'t N, leaf: &'t N) -> Probability {
+        //? gotta optimize out integration over shared ancestors that cancels out in this division. Node: Eq? Hash?
+        self.expected_reach(leaf) / self.expected_reach(root)
+    }
+    fn sampling_reach(&self, _: &'t N, _: &'t N) -> Probability {
+        1.0
+    }
+}
+
+/// an optimizer updates profile to minimize regret, and updates regrets from existing profiles.
+trait Optimizer<'t, P, S, D, I, N, V, A, B, C>
+where
+    P: Profile<'t, S, D, N, V, A, B, C>,
+    S: Strategy<'t, D, A, B>,
+    D: Distribution<'t, A>,
+    I: Info<'t, N, V, A, B, C>,
+    N: Node<'t, V, A, B, C>,
+    V: Vertex<'t, A, B, C>,
+    A: Action<'t>,
+    B: Bucket<'t>,
+    C: Player<'t>,
+{
+    // required
+    fn profile(&self) -> &P;
+    fn update_regret(&mut self, info: &I);
+    fn update_policy(&mut self, info: &I);
+    fn current_regret(&self, info: &'t I, action: &'t A) -> Utility;
+    fn instant_regret(&self, info: &'t I, action: &'t A) -> Utility {
+        info.roots()
+            .iter()
+            .map(|root| self.profile().gain(root, action))
+            .sum::<Utility>()
+    }
+    fn pending_regret(&self, info: &'t I, action: &'t A) -> Utility {
+        self.instant_regret(info, action) + self.current_regret(info, action)
+    }
+    fn policy_vector(&self, info: &'t I) -> Vec<(A, Probability)> {
+        let regrets = info
+            .roots()
+            .iter()
+            .map(|root| root.outgoing())
+            .flatten()
+            .map(|action| (*action, self.current_regret(info, action)))
+            .map(|(a, r)| (a, r.max(Utility::MIN_POSITIVE)))
+            .collect::<Vec<(A, Probability)>>();
+        let sum = regrets.iter().map(|(_, r)| r).sum::<Utility>();
+        let policy = regrets.into_iter().map(|(a, r)| (a, r / sum)).collect();
+        policy
+    }
+    fn regret_vector(&self, info: &'t I) -> Vec<(A, Utility)> {
+        info.outgoing()
+            .iter()
+            .map(|action| (**action, self.pending_regret(info, action)))
+            .collect()
+    }
+}
+
+/// trainer will update regrets and profile in a mutable loop
+trait Trainer<'t, O, P, S, D, T, I, N, V, A, B, C>
+where
+    O: Optimizer<'t, P, S, D, I, N, V, A, B, C>,
+    P: Profile<'t, S, D, N, V, A, B, C>,
+    S: Strategy<'t, D, A, B>,
+    D: Distribution<'t, A>,
+    T: Tree<'t, I, N, V, A, B, C>,
+    I: Info<'t, N, V, A, B, C>,
+    N: Node<'t, V, A, B, C>,
+    V: Vertex<'t, A, B, C>,
+    A: Action<'t>,
+    B: Bucket<'t>,
+    C: Player<'t>,
+{
+    // required
+    fn train(&mut self, n: usize);
+}
