87325c826f3690d8c800ba9a41d777d97239e2fa
Kelechi Ukah <ukahkelechi@gmail.com>
2024-06-22 01:17:55 -0400

    refactor weird inbetween of complete leaf sampling vs MC sampling

--- src/cfr/profile/profile.rs
+++ src/cfr/profile/profile.rs
@@ -4,9 +4,9 @@ use crate::cfr::tree::rps::bucket::Bucket;
 use crate::cfr::tree::rps::node::Node;
 use crate::cfr::tree::rps::player::Player;
 use crate::Probability;
-use crate::Utility;
 use std::collections::HashMap;
 
+//? don't love how epoch is contagious across Trainer < Minimizer < Profile > >
 pub struct Profile(pub HashMap<Bucket, Policy>);
 
 impl Profile {
@@ -38,106 +38,8 @@ impl Profile {
             .insert(edge, value);
     }
 
-    // marginal counterfactual gain over strategy EV
-    pub fn gain(&self, root: &Node, edge: &Edge) -> Utility {
-        let cfactual = self.cfactual_value(root, edge);
-        let expected = self.expected_value(root);
-        cfactual - expected
-    }
-
-    #[allow(dead_code)]
-    // recursive sampling methods
-    fn leaves<'a>(&self, node: &'a Node) -> Vec<&'a Node> {
-        match node.children().len() {
-            0 => vec![&node],
-            _ => node
-                .children()
-                .iter()
-                .map(|child| self.leaves(child))
-                .flatten()
-                .collect(),
-        }
-    }
-    fn sample<'a>(&self, node: &'a Node) -> Vec<&'a Node> {
-        // external sampling explores ALL possible actions for the traverser and ONE possible action for chance & opps
-        let traverser = node.player(); //  self.traverser: &Player
-        if 0 == node.children().len() {
-            vec![&node]
-        } else if traverser == node.player() {
-            self.expand(node)
-        } else {
-            self.select(node)
-        }
-    }
-    fn expand<'a>(&self, node: &'a Node) -> Vec<&'a Node> {
-        // implicitly we're at a node belonging to the traverser
-        node.children()
-            .iter()
-            .map(|child| self.sample(child))
-            .flatten()
-            .collect()
-    }
-    fn select<'a>(&self, node: &'a Node) -> Vec<&'a Node> {
-        // now wer'e at an opp or chance node
-        use rand::distributions::Distribution;
-        use rand::distributions::WeightedIndex;
-        let mut rng = rand::thread_rng();
-        let ref weights = node
-            .outgoing()
-            .iter()
-            .map(|edge| self.weight(node, edge))
-            .collect::<Vec<Probability>>();
-        let distribution = WeightedIndex::new(weights).expect("same length");
-        let index = distribution.sample(&mut rng);
-        let child = *node.children().get(index).expect("valid index");
-        self.sample(child)
-    }
-
-    // utility calculations
-    fn cfactual_value(&self, root: &Node, edge: &Edge) -> Utility {
-        1.0 * self.cfactual_reach(root)
-            * self
-                .sample(root.follow(edge))
-                .iter() //                                  duplicated calculation
-                .map(|leaf| self.relative_value(root, leaf))
-                .sum::<Utility>()
-    }
-    fn expected_value(&self, root: &Node) -> Utility {
-        1.0 * self.expected_reach(root)
-            * self
-                .sample(root)
-                .iter() //                                  duplicated calculation
-                .map(|leaf| self.relative_value(root, leaf))
-                .sum::<Utility>()
-    }
-    fn relative_value(&self, root: &Node, leaf: &Node) -> Utility {
-        1.0 * self.relative_reach(root, leaf)
-            * self.sampling_reach(root, leaf)
-            * self.terminal_value(root, leaf)
-    }
-    fn terminal_value(&self, root: &Node, leaf: &Node) -> Utility {
-        const HI_STAKES: Utility = 2e0; // we can modify payoffs to verify convergence
-        const LO_STAKES: Utility = 1e0;
-        let direction = match root.player() {
-            Player::P1 => 0. + 1.,
-            Player::P2 => 0. - 1.,
-            _ => unreachable!("payoff should not be queried for chance"),
-        };
-        let payoff = match leaf.data.0 {
-            04 | 08 | 12 => 0.0,
-            07 => 0. + LO_STAKES, // P > R
-            05 => 0. - LO_STAKES, // R < P
-            06 => 0. + HI_STAKES, // R > S
-            11 => 0. + HI_STAKES, // S > P
-            10 => 0. - HI_STAKES, // S < R
-            09 => 0. - HI_STAKES, // P < S
-            _ => unreachable!("eval at terminal node, depth > 1"),
-        };
-        direction * payoff
-    }
-
     // probability calculations
-    fn weight(&self, node: &Node, edge: &Edge) -> Probability {
+    pub fn weight(&self, node: &Node, edge: &Edge) -> Probability {
         match node.player() {
             Player::Chance => {
                 let n = node.outgoing().len();
@@ -149,7 +51,7 @@ impl Profile {
             }
         }
     }
-    fn cfactual_reach(&self, root: &Node) -> Probability {
+    pub fn cfactual_reach(&self, root: &Node) -> Probability {
         let mut prod = 1.0;
         let mut next = root;
         while let Some(from) = next.parent() {
@@ -164,7 +66,7 @@ impl Profile {
         }
         prod
     }
-    fn expected_reach(&self, node: &Node) -> Probability {
+    pub fn expected_reach(&self, node: &Node) -> Probability {
         match node.parent() {
             None => 1.0,
             Some(from) => {
@@ -173,7 +75,7 @@ impl Profile {
             }
         }
     }
-    fn relative_reach(&self, root: &Node, leaf: &Node) -> Probability {
+    pub fn relative_reach(&self, root: &Node, leaf: &Node) -> Probability {
         if root.bucket() == leaf.bucket() {
             1.0
         } else {
@@ -182,7 +84,7 @@ impl Profile {
             self.weight(node, from) * self.relative_reach(root, node)
         }
     }
-    fn sampling_reach(&self, _: &Node, _: &Node) -> Probability {
+    pub fn sampling_reach(&self, _: &Node, _: &Node) -> Probability {
         1.0 / 1.0
     }
 }
--- src/cfr/training/minimizer.rs
+++ src/cfr/training/minimizer.rs
@@ -1,20 +1,37 @@
 use crate::cfr::profile::profile::Profile;
 use crate::cfr::tree::rps::action::Edge;
 use crate::cfr::tree::rps::info::Info;
+use crate::cfr::tree::rps::node::Node;
+use crate::cfr::tree::rps::player::Player;
 use crate::cfr::tree::rps::tree::Tree;
 use crate::Probability;
 use crate::Utility;
 
+type Epoch = usize;
+
 pub struct Minimizer {
-    time: usize,
+    epoch: Epoch,
     regrets: Profile,
     current: Profile,
     average: Profile,
+    traverser: Player,
 }
 impl Minimizer {
+    pub fn report(&self) {
+        if self.epoch % 1_000 == 0 {
+            println!("T{}", self.epoch);
+            for (bucket, strategy) in self.average().0.iter() {
+                for (action, weight) in strategy.0.iter() {
+                    println!("Bucket {:?}  {:?}: {:.4?}", bucket, action, weight);
+                }
+                break;
+            }
+        }
+    }
     pub fn average(&self) -> &Profile {
         &self.average
     }
+    #[allow(dead_code)]
     pub fn current(&self) -> &Profile {
         &self.current
     }
@@ -34,14 +51,30 @@ impl Minimizer {
             }
         }
         Self {
-            time: 0,
+            epoch: 0,
             average,
             current,
             regrets,
+            traverser: Player::P1,
         }
     }
+
     // mutating update methods at each infoset
+    pub fn update_epoch(&mut self, t: Epoch) {
+        self.epoch = t;
+        match self.epoch % 2 {
+            0 => self.traverser = Player::P1,
+            _ => self.traverser = Player::P2,
+        }
+    }
     pub fn update_regret(&mut self, info: &Info) {
+        // bail if the traverser is not the player at the infoset
+        if &self.traverser != info.sample().player() {
+            return;
+            //? TODO weird duplication of traverser check
+            //? TODO weird duplication of traverser check
+        }
+        //? TODO weird duplication of traverser check
         for (ref action, regret) in self.regret_vector(info) {
             let bucket = info.sample().bucket();
             let running = self.regrets.get_mut(bucket, action);
@@ -49,18 +82,26 @@ impl Minimizer {
         }
     }
     pub fn update_policy(&mut self, info: &Info) {
+        // bail if the traverser is not the player at the infoset
+        if &self.traverser != info.sample().player() {
+            return;
+            //? TODO weird duplication of traverser check
+            //? TODO weird duplication of traverser check
+        }
+        //? TODO weird duplication of traverser check
         for (ref action, weight) in self.policy_vector(info) {
             let bucket = info.sample().bucket();
             let current = self.current.get_mut(bucket, action);
             let average = self.average.get_mut(bucket, action);
             *current = weight;
-            *average *= self.time as Probability;
+            *average *= self.epoch as Probability;
             *average += weight;
-            *average /= self.time as Probability + 1.;
+            *average /= self.epoch as Probability + 1.;
         }
-        self.time += 1;
     }
+
     // policy calculation via cumulative regrets
+    // regret calculation via regret matching +
     fn policy_vector(&self, info: &Info) -> Vec<(Edge, Probability)> {
         let regrets = info
             .sample()
@@ -73,7 +114,6 @@ impl Minimizer {
         let policy = regrets.into_iter().map(|(a, r)| (a, r / sum)).collect();
         policy
     }
-    // regret calculation via regret matching +
     fn regret_vector(&self, info: &Info) -> Vec<(Edge, Utility)> {
         info.sample()
             .outgoing()
@@ -81,6 +121,7 @@ impl Minimizer {
             .map(|action| (**action, self.matched_regret(info, action)))
             .collect()
     }
+
     // regret storge and calculation
     fn matched_regret(&self, info: &Info, action: &Edge) -> Utility {
         let running = self.running_regret(info, action);
@@ -95,7 +136,106 @@ impl Minimizer {
     fn instant_regret(&self, info: &Info, action: &Edge) -> Utility {
         info.roots()
             .iter()
-            .map(|root| self.current().gain(root, action))
+            .map(|root| self.gain(root, action))
             .sum::<Utility>()
+    }
+
+    // marginal counterfactual gain over strategy EV
+    fn gain(&self, root: &Node, edge: &Edge) -> Utility {
+        let cfactual = self.cfactual_value(root, edge);
+        let expected = self.expected_value(root);
+        cfactual - expected
+    }
+    fn cfactual_value(&self, root: &Node, edge: &Edge) -> Utility {
+        1.0 * self.current.cfactual_reach(root)
+            * self
+                .leaves(root.follow(edge))
+                .iter() //                                  duplicated calculation
+                .map(|leaf| self.relative_value(root, leaf))
+                .sum::<Utility>()
+    }
+    fn expected_value(&self, root: &Node) -> Utility {
+        1.0 * self.current.expected_reach(root)
+            * self
+                .leaves(root)
+                .iter() //                                  duplicated calculation
+                .map(|leaf| self.relative_value(root, leaf))
+                .sum::<Utility>()
+    }
+    fn relative_value(&self, root: &Node, leaf: &Node) -> Utility {
+        1.0 * self.current.relative_reach(root, leaf)
+            * self.current.sampling_reach(root, leaf)
+            * self.terminal_value(root, leaf)
+    }
+    fn terminal_value(&self, root: &Node, leaf: &Node) -> Utility {
+        const HI_STAKES: Utility = 2e0; // we can modify payoffs to verify convergence
+        const LO_STAKES: Utility = 1e0;
+        let direction = match root.player() {
+            Player::P1 => 0. + 1.,
+            Player::P2 => 0. - 1.,
+            _ => unreachable!("payoff should not be queried for chance"),
+        };
+        let payoff = match leaf.data.0 {
+            04 | 08 | 12 => 0.0,
+            07 => 0. + LO_STAKES, // P > R
+            05 => 0. - LO_STAKES, // R < P
+            06 => 0. + HI_STAKES, // R > S
+            11 => 0. + HI_STAKES, // S > P
+            10 => 0. - HI_STAKES, // S < R
+            09 => 0. - HI_STAKES, // P < S
+            _ => unreachable!("eval at terminal node, depth > 1"),
+        };
+        direction * payoff
+    }
+
+    // recursive sampling methods
+    fn leaves<'a>(&self, node: &'a Node) -> Vec<&'a Node> {
+        match node.children().len() {
+            0 => vec![&node],
+            _ => node
+                .children()
+                .iter()
+                .map(|child| self.leaves(child))
+                .flatten()
+                .collect(),
+        }
+    }
+    #[allow(dead_code)]
+    fn sample<'a>(&self, node: &'a Node) -> Vec<&'a Node> {
+        // external sampling explores ALL possible actions for the traverser and ONE possible action for chance & opps
+        let ref traverser = self.traverser;
+        if 0 == node.children().len() {
+            vec![&node]
+        } else if traverser == node.player() {
+            self.explore_all(node)
+        } else {
+            self.explore_one(node) // external sampling is weird
+        }
+    }
+    #[allow(dead_code)]
+    fn explore_all<'a>(&self, node: &'a Node) -> Vec<&'a Node> {
+        // implicitly we're at a node belonging to the traverser
+        node.children()
+            .iter()
+            .map(|child| self.sample(child))
+            .flatten()
+            .collect()
+    }
+    #[allow(dead_code)]
+    fn explore_one<'a>(&self, node: &'a Node) -> Vec<&'a Node> {
+        // now wer'e at an opp or chance node
+        use rand::distributions::Distribution;
+        use rand::distributions::WeightedIndex;
+
+        let ref mut rng = rand::thread_rng();
+        let ref weights = node
+            .outgoing()
+            .iter()
+            .map(|edge| self.current.weight(node, edge))
+            .collect::<Vec<Probability>>();
+        let distribution = WeightedIndex::new(weights).expect("same length");
+        let index = distribution.sample(rng);
+        let child = node.children().remove(index); // kidnapped!
+        self.sample(child)
     }
 }
