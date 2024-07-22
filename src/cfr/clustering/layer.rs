44942b9916b6ad81df6b7468f4b50bd2c179c61e
Kelechi Ukah <ukahkelechi@gmail.com>
2024-07-21 20:51:24 -0400

    remove progress logs from river generation

--- src/cfr/clustering/layer.rs
+++ src/cfr/clustering/layer.rs
@@ -232,15 +232,7 @@ impl River {
     fn clusters() -> HashMap<Observation, Abstraction> {
         Observation::predecessors(Street::Show)
             .into_iter()
-            .enumerate()
-            .map(|(i, obs)| {
-                if i % 1000 == 0 {
-                    println!("{:<10}", i);
-                    return (obs, Abstraction::from(obs.equity()));
-                } else {
-                    return (obs, Abstraction::from(obs.equity()));
-                }
-            })
+            .map(|obs| (obs, Abstraction::from(obs.equity())))
             .collect::<HashMap<_, _>>()
     }
 }
