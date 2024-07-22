5fe04d949fc291aea73614f0de150fcf6c210835
Kelechi Ukah <ukahkelechi@gmail.com>
2024-07-21 21:08:31 -0400

    Layer::save semantics

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,29 @@ mod players;
 pub type Utility = f32;
 pub type Probability = f32;
 
-#[allow(unused_variables)]
-fn main() {
+#[tokio::main]
+async fn main() {
+    // Abstraction generation
     let ref river = Layer::river();
     let ref turn = Layer::upper(river);
     let ref flop = Layer::upper(turn);
     let ref pref = Layer::upper(flop);
 
+    // Postgres connection semantics
+    // I'm only ::clone() for visual parity tbh
+    let ref url = std::env::var("DB_CONNECTION")
+        .expect("missing enironment: DB_CONNECTION")
+        .clone();
+    let ref pool = sqlx::PgPool::connect(url)
+        .await
+        .expect("database connection");
+
+    // Async persistence
+    river.save(pool).await;
+    turn.save(pool).await;
+    flop.save(pool).await;
+    pref.save(pool).await;
+
+    // CFR training iterations
     Solver::new().solve(50_000);
 }
