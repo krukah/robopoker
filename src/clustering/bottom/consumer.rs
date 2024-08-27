9647255b7c897d271c60a2bc6a7fd9f9f5051b59
Kelechi Ukah <ukahkelechi@gmail.com>
2024-08-27 00:44:56 -0400

    move everything into memory! it was nice while it lasted, postgrse COPY FROM! we'll meet v soon

--- src/clustering/bottom/consumer.rs
+++ src/clustering/bottom/consumer.rs
@@ -1,83 +1,30 @@
 use super::progress::Progress;
 use crate::clustering::bottom::Abstraction;
 use crate::clustering::bottom::Observation;
-use std::pin::Pin;
+use crate::clustering::upper::histogram::Histogram;
+use std::collections::HashMap;
 use tokio::sync::mpsc::Receiver;
-use tokio_postgres::binary_copy::BinaryCopyInWriter;
-use tokio_postgres::types::Type;
-
-const ROWS: &'static [Type] = &[Type::INT8, Type::INT8];
-const INIT: &'static str = r#"
-            BEGIN;
-            CREATE UNLOGGED TABLE IF NOT EXISTS centroid (
-                observation BIGINT PRIMARY KEY,
-                abstraction BIGINT,
-            );
-            CREATE UNLOGGED TABLE IF NOT EXISTS distance (
-                xor         BIGINT PRIMARY KEY,
-                distance    FLOAT,
-            );
-            TRUNCATE TABLE centroid;
-            TRUNCATE TABLE distance;
-        "#;
-const COPY: &'static str = r#"
-            COPY centroid (
-                observation,
-                abstraction
-            )
-            FROM STDIN BINARY FREEZE;
-        "#;
 
 pub struct Consumer {
     rx: Receiver<(Observation, Abstraction)>,
-    writer: BinaryCopyInWriter,
-    client: tokio_postgres::Client,
+    table: HashMap<Observation, (Histogram, Abstraction)>,
 }
 
 impl Consumer {
     pub async fn new(rx: Receiver<(Observation, Abstraction)>) -> Self {
-        let ref url = std::env::var("DATABASE_URL").expect("DATABASE_URL in environment");
-        let (client, connection) = tokio_postgres::connect(url, tokio_postgres::NoTls)
-            .await
-            .expect("connect to database");
-        tokio::spawn(connection);
-        client
-            .batch_execute(INIT)
-            .await
-            .expect("create and truncate tables");
-        let sink = client
-            .copy_in(COPY)
-            .await
-            .expect("get sink for COPY transaction");
-        let writer = BinaryCopyInWriter::new(sink, ROWS);
-        Self { rx, client, writer }
+        Self {
+            rx,
+            table: HashMap::new(),
+        }
     }
 
-    pub async fn run(mut self) {
-        let ref mut writer = self.writer;
-        let mut writer = unsafe { Pin::new_unchecked(writer) };
+    pub async fn run(mut self) -> HashMap<Observation, (Histogram, Abstraction)> {
         let mut progress = Progress::new();
-        while let Some((obs, abs)) = self.rx.recv().await {
+        while let Some((observation, abstraction)) = self.rx.recv().await {
             progress.tick();
-            // MARK
-            // this would be where we inset into a HashMap<Obs, Abs>]
-            // self.observations.insert(obs, abs);
-            let ref street = obs.street() as i8;
-            let ref observation = i64::from(obs);
-            let ref abstraction = i64::from(abs);
-            writer
-                .as_mut()
-                .write(&[street, observation, abstraction])
-                .await
-                .expect("write row into heap");
+            let histogram = Histogram::witness(Histogram::default(), abstraction.clone());
+            self.table.insert(observation, (histogram, abstraction));
         }
-        writer
-            .finish()
-            .await
-            .expect("complete 2.8B rows of COPY transaction");
-        self.client
-            .execute("COMMIT", &[])
-            .await
-            .expect("commit transaction");
+        self.table
     }
 }
--- src/clustering/bottom/mod.rs
+++ src/clustering/bottom/mod.rs
@@ -19,15 +19,15 @@ pub async fn upload() {
     let mut tasks = Vec::with_capacity(n);
     let ref observations = Arc::new(Observation::all(Street::Rive));
     let (tx, rx) = tokio::sync::mpsc::channel::<(Observation, Abstraction)>(1024);
-    let consumer = Consumer::new(rx).await;
-    tasks.push(tokio::spawn(consumer.run()));
+    let consumer = tokio::spawn(Consumer::new(rx).await.run());
     for task in 0..n {
         let tx = tx.clone();
         let observations = observations.clone();
-        let producer = Producer::new(task, tx, observations);
-        tasks.push(tokio::task::spawn(producer.run()));
+        let producer = tokio::spawn(Producer::new(task, tx, observations).run());
+        tasks.push(producer);
     }
     futures::future::join_all(tasks).await;
+    consumer.await.expect("consumer task completes");
 }
 
 pub async fn download() -> Layer {
