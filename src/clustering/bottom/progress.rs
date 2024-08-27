22609ad126a3c748a25edff59de76bfea9d30071
Kelechi Ukah <ukahkelechi@gmail.com>
2024-08-27 18:48:59 -0400

    db persistence solved, progress update

--- src/clustering/bottom/progress.rs
+++ src/clustering/bottom/progress.rs
@@ -2,39 +2,39 @@ use tokio::time::Instant;
 
 /// A struct to track and display progress of a long-running operation.
 pub struct Progress {
+    total: usize,
+    ticks: usize,
     begin: Instant,
     delta: Instant,
-    complete: u32,
 }
 impl Progress {
-    const CHECKPOINT: usize = 10_000;
-    const TOTAL: usize = 2_809_475_760;
-
-    pub fn new() -> Self {
+    const CHECKPOINT: usize = 1_000_000;
+    pub fn new(total: usize) -> Self {
         let now = Instant::now();
         Self {
+            total,
             begin: now,
             delta: now,
-            complete: 0,
+            ticks: 0,
         }
     }
     pub fn tick(&mut self) {
-        self.complete += 1;
-        if self.complete % Self::CHECKPOINT as u32 == 0 {
+        self.ticks += 1;
+        if self.ticks % Self::CHECKPOINT == 0 {
             use std::io::Write;
             let now = Instant::now();
             let total_t = now.duration_since(self.begin);
             let delta_t = now.duration_since(self.delta);
             self.delta = now;
-            print!("\x1B[4A"); // Move cursor up 4 lines (for 4 lines of output)
-            print!("\x1B[0J"); // Clear from cursor to end of screen
-            println!("Elapsed: {:.0?}", total_t);
-            #[rustfmt::skip]
-            println!("Mean Freq:{:>10.0}", self.complete as f32 / total_t.as_secs_f32());
-            #[rustfmt::skip]
-            println!("Last Freq:{:>10.0}", Self::CHECKPOINT as f32 / delta_t.as_secs_f32());
-            #[rustfmt::skip]
-            println!("{:10}{:>10.1}%", self.complete, (self.complete as f32 / Self::TOTAL as f32) * 100.0);
+            print!("\r"); // Move cursor to the beginning of the line
+            print!("\x1B[K"); // Clear the line
+            print!(
+                "Elapsed: {:8.0?} | Mean Freq: {:10.0} | Last Freq: {:10.0} | Progress: {:6.2}%",
+                total_t,
+                self.ticks as f32 / total_t.as_secs_f32(),
+                Self::CHECKPOINT as f32 / delta_t.as_secs_f32(),
+                (self.ticks as f32 / self.total as f32) * 100.0
+            );
             std::io::stdout().flush().unwrap();
         }
     }
