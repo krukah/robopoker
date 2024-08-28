d37cf8960917498fbd54acd8b928e829364d7a87
Kelechi Ukah <ukahkelechi@gmail.com>
2024-08-28 18:02:41 -0400

    sweet nothings

--- src/clustering/upper/metric.rs
+++ src/clustering/upper/metric.rs
@@ -10,28 +10,28 @@ use std::collections::HashMap;
 /// essential for clustering algorithms and comparing distributions.
 pub trait Metric {
     fn emd(&self, x: &Histogram, y: &Histogram) -> f32;
-    fn similarity(&self, x: &Abstraction, y: &Abstraction) -> f32;
+    fn distance(&self, x: &Abstraction, y: &Abstraction) -> f32;
 }
 impl Metric for HashMap<Pair, f32> {
     fn emd(&self, x: &Histogram, y: &Histogram) -> f32 {
         let n = x.domain().len();
         let m = y.domain().len();
         let mut cost = 0.0;
-        let mut targets = x
-            .domain()
-            .iter()
-            .map(|&a| (a, 1.0 / n as f32))
-            .collect::<HashMap<&Abstraction, f32>>();
         let mut removed = x
             .domain()
             .iter()
             .map(|&a| (a, false))
             .collect::<HashMap<&Abstraction, bool>>();
+        let mut targets = x
+            .domain()
+            .iter()
+            .map(|&a| (a, 1.0 / n as f32))
+            .collect::<HashMap<&Abstraction, f32>>();
         let mut remains = y
             .domain()
             .iter()
             .map(|&a| (a, y.weight(a)))
-            .collect::<HashMap<&Abstraction, f32>>();
+            .collect::<HashMap<&Abstraction, f32>>(); // this is effectively a clone
         for _ in 0..m {
             for supplier in x.domain() {
                 if removed
@@ -44,7 +44,7 @@ impl Metric for HashMap<Pair, f32> {
                 let (ref neighbor, nearest) = y
                     .domain()
                     .iter()
-                    .map(|candidate| (candidate.to_owned(), self.similarity(supplier, candidate)))
+                    .map(|candidate| (candidate.to_owned(), self.distance(supplier, candidate)))
                     .min_by(|&(_, ref a), &(_, ref b)| a.partial_cmp(b).expect("not NaN"))
                     .expect("receiver domain is empty");
                 let target = targets
@@ -68,12 +68,12 @@ impl Metric for HashMap<Pair, f32> {
                         .expect("yabs not found in remains mass") = 0.0;
                 } else {
                     cost += target * nearest;
-                    *remains
-                        .get_mut(neighbor)
-                        .expect("yabs not found in remains mass") -= target;
                     *targets
                         .get_mut(supplier)
                         .expect("xabs not found in targets mass") = 0.0;
+                    *remains
+                        .get_mut(neighbor)
+                        .expect("yabs not found in remains mass") -= target;
                     *removed
                         .get_mut(supplier)
                         .expect("xabs not found in removed mass") = true;
@@ -82,7 +82,7 @@ impl Metric for HashMap<Pair, f32> {
         }
         cost
     }
-    fn similarity(&self, x: &Abstraction, y: &Abstraction) -> f32 {
+    fn distance(&self, x: &Abstraction, y: &Abstraction) -> f32 {
         let ref xor = Pair::from((x, y));
         self.get(xor).expect("precalculated distance").clone()
     }
