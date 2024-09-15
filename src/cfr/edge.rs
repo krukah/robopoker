e984e251dd78ca0332a9991cf9919b7e4c78bdf5
Kelechi Ukah <ukahkelechi@gmail.com>
2024-09-14 23:42:14 -0400

    market traits for generic CFR

--- src/cfr/edge.rs
+++ src/cfr/edge.rs
@@ -1,6 +1,24 @@
+use std::hash::Hash;
+
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq, Ord, PartialOrd)]
 pub enum Edge {
     RO,
     PA,
     SC,
 }
+
+#[allow(unused)]
+trait CFREdge
+where
+    Self: Sized,
+    Self: Clone,
+    Self: Copy,
+    Self: Hash,
+    Self: Eq,
+    Self: PartialEq,
+    Self: Ord,
+    Self: PartialOrd,
+{
+}
+
+impl CFREdge for Edge {}
--- src/cfr/player.rs
+++ src/cfr/player.rs
@@ -1,6 +1,21 @@
+use std::hash::Hash;
+
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
 pub enum Player {
     P1,
     P2,
     Chance,
 }
+
+#[allow(unused)]
+trait CFRPlayer
+where
+    Self: Sized,
+    Self: Clone,
+    Self: Copy,
+    Self: Hash,
+    Self: Eq,
+{
+}
+
+impl CFRPlayer for Player {}
