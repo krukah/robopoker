42f11e5e748bf911cdc8be49389608ff3cf67b25
Kelechi Ukah <ukahkelechi@gmail.com>
2024-10-06 19:07:44 -0400

    eliminate RwLock deadlock; don't hog the write lock too long

--- src/mccfr/bucket.rs
+++ src/mccfr/bucket.rs
@@ -1,11 +1,22 @@
+use super::edge::Edge;
 use crate::clustering::abstraction::Abstraction;
 use std::hash::Hash;
 
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq, Ord, PartialOrd)]
 pub struct Path(u64);
 
+impl From<Vec<Edge>> for Path {
+    fn from(actions: Vec<Edge>) -> Self {
+        todo!()
+    }
+}
+
+/// the product of
+/// "information abstraction" and
+/// "action absraction" are what we index the (regret, strategy, average, ...) on
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq, Ord, PartialOrd)]
 pub struct Bucket(Path, Abstraction);
+
 impl Bucket {
     pub fn root() -> Bucket {
         return todo!("what is the bucket of the root node, what evn is the root node??");
