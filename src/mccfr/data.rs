23046600377b41971b8492dabef48264832b2b07
Kelechi Ukah <ukahkelechi@gmail.com>
2024-11-09 15:06:15 -0500

    put Bucket back into Data

--- src/mccfr/data.rs
+++ src/mccfr/data.rs
@@ -1,16 +1,16 @@
-use super::bucket::Recall;
+use super::bucket::Bucket;
 use crate::mccfr::player::Player;
 use crate::play::game::Game;
 
 #[derive(Debug)]
 pub struct Data {
     game: Game,
-    past: Recall,
+    info: Bucket,
 }
 
-impl From<(Game, Recall)> for Data {
-    fn from((game, past): (Game, Recall)) -> Self {
-        Self { game, past }
+impl From<(Game, Bucket)> for Data {
+    fn from((game, info): (Game, Bucket)) -> Self {
+        Self { game, info }
     }
 }
 
@@ -18,8 +18,11 @@ impl Data {
     pub fn game(&self) -> &Game {
         &self.game
     }
-    pub fn recall(&self) -> &Recall {
-        &self.past
+    // pub fn recall(&self) -> &Recall {
+    //     &self.past
+    // }
+    pub fn bucket(&self) -> &Bucket {
+        &self.info
     }
     pub fn player(&self) -> Player {
         Player(self.game.player())
