ad208edccf96e47764a70c2860851afef66a1a7e
Kelechi Ukah <ukahkelechi@gmail.com>
2024-11-17 23:51:50 -0500

    nothings

--- src/transport/support.rs
+++ src/transport/support.rs
@@ -3,5 +3,5 @@
 ///
 /// currently only implemented by
 /// - Abstraction::Random(_) , where Histogram is the implied Density and Metric is the implied Measure
-/// - Abstraction::Equity(_) , where Histogram is the implied Density and Equity is the implied Measure
+/// - Abstraction::Equity(_) , where Histogram is the implied Density and i16    is the implied Measure
 pub trait Support {}
