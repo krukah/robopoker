ecaf0fe31ff75c721c8b4e0330494d4ba38fbfb5
Kelechi Ukah <ukahkelechi@gmail.com>
2024-12-21 00:29:34 -0500

    abstraction defined by Street, Index now

--- src/clustering/abstraction.rs
+++ src/clustering/abstraction.rs
@@ -1,7 +1,6 @@
-use crate::cards::hand::Hand;
-use crate::cards::hole::Hole;
-use crate::cards::observation::Observation;
+use crate::cards::street::Street;
 use crate::transport::support::Support;
+use crate::Arbitrary;
 use crate::Probability;
 use std::hash::Hash;
 use std::u64;
@@ -13,56 +12,86 @@ use std::u64;
 /// - Other Streets: we use a u64 to represent the hash signature of the centroid Histogram over lower layers of abstraction.
 #[derive(Copy, Clone, Hash, Eq, PartialEq, Debug, PartialOrd, Ord)]
 pub enum Abstraction {
-    Percent(u8),  // river
+    Percent(u64), // river
     Learned(u64), // flop, turn
     Preflop(u64), // preflop
 }
 
 impl Abstraction {
-    pub fn random() -> Self {
-        loop {
-            let x = rand::random::<u64>();
-            match x >> 52 {
-                Self::POCKET_TAG => continue,
-                Self::EQUITY_TAG => continue,
-                _ => return Self::Learned(x),
-            }
-        }
-    }
-    pub const fn range() -> &'static [Self] {
-        &Self::BUCKETS
-    }
+    const N: usize = crate::KMEANS_EQTY_CLUSTER_COUNT - 1;
     pub const fn size() -> usize {
         Self::N as usize + 1
     }
-
-    fn hash(n: u64) -> u64 {
-        Self::mask(n.wrapping_mul(Self::FIBONACCI))
+    pub fn random() -> Self {
+        use rand::Rng;
+        let street = Street::random();
+        let n = street.k();
+        let i = rand::thread_rng().gen_range(0..n);
+        Self::from((street, i))
     }
-    fn mask(n: u64) -> u64 {
-        n & 0x000FFFFFFFFFFFFF
+    pub fn range() -> impl Iterator<Item = Self> {
+        (0..=Self::N).map(|i| Self::from((Street::Rive, i as usize)))
     }
-    fn quantize(p: Probability) -> u8 {
-        (p * Probability::from(Self::N)).round() as u8
-    }
-    fn floatize(q: u8) -> Probability {
-        Probability::from(q) / Probability::from(Self::N)
-    }
-
-    const N: u8 = 63;
-    const FIBONACCI: u64 = 0x9E3779B97F4A7C15;
-    const EQUITY_TAG: u64 = 0xEEE;
-    const POCKET_TAG: u64 = 0xFFF;
-    const BUCKETS: [Self; Self::size()] = Self::buckets();
-
-    const fn buckets() -> [Self; Self::size()] {
-        let mut buckets = [Self::Percent(0); Self::size()];
-        let mut i = 0;
-        while i <= Self::N {
-            buckets[i as usize] = Self::Percent(i as u8);
-            i += 1;
+    pub fn street(&self) -> Street {
+        match self {
+            Abstraction::Percent(n) | Abstraction::Learned(n) | Abstraction::Preflop(n) => {
+                match n >> 56 {
+                    0 => Street::Pref,
+                    1 => Street::Flop,
+                    2 => Street::Turn,
+                    3 => Street::Rive,
+                    _ => panic!("at the disco"),
+                }
+            }
         }
-        buckets
+    }
+    pub fn index(&self) -> usize {
+        match self {
+            Abstraction::Percent(n) | Abstraction::Learned(n) | Abstraction::Preflop(n) => {
+                (n & 0xFFFFFFFFFFFFFF) as usize
+            }
+        }
+    }
+
+    fn quantize(p: Probability) -> usize {
+        (p * Self::N as Probability).round() as usize
+    }
+    fn floatize(q: usize) -> Probability {
+        q as Probability / Self::N as Probability
+    }
+}
+
+impl From<(Street, usize)> for Abstraction {
+    fn from((street, index): (Street, usize)) -> Self {
+        let bits = ((street as u8 as u64) << 52) | (index as u64);
+        match street {
+            Street::Pref => Abstraction::Preflop(bits),
+            Street::Flop => Abstraction::Learned(bits),
+            Street::Turn => Abstraction::Learned(bits),
+            Street::Rive => Abstraction::Percent(bits),
+        }
+    }
+}
+
+/// probability isomorphism
+///
+/// for river, we use a u8 to represent the equity bucket,
+/// i.e. Equity(0) is the 0% equity bucket,
+/// and Equity(N) is the 100% equity bucket.
+impl From<Probability> for Abstraction {
+    fn from(p: Probability) -> Self {
+        assert!(p >= 0.);
+        assert!(p <= 1.);
+        Self::from((Street::Rive, Self::quantize(p)))
+    }
+}
+impl From<Abstraction> for Probability {
+    fn from(abstraction: Abstraction) -> Self {
+        match abstraction {
+            Abstraction::Percent(_) => Abstraction::floatize(abstraction.index()),
+            Abstraction::Learned(_) => unreachable!("no cluster into probability"),
+            Abstraction::Preflop(_) => unreachable!("no preflop into probability"),
+        }
     }
 }
 
@@ -71,19 +100,23 @@ impl Abstraction {
 /// conversion to u64 for SQL storage.
 impl From<Abstraction> for u64 {
     fn from(a: Abstraction) -> Self {
+        // let street = a.street();
+        // let index = a.index();
+        // let bits = ((street as u8 as u64) << 52) | (index as u64);
+        // bits
         match a {
-            Abstraction::Learned(n) => n,
-            Abstraction::Percent(e) => (Abstraction::EQUITY_TAG << 52) | (e as u64 & 0xFF) << 44,
-            Abstraction::Preflop(h) => (Abstraction::POCKET_TAG << 52) | (Abstraction::mask(h)),
+            Abstraction::Percent(n) | Abstraction::Learned(n) | Abstraction::Preflop(n) => n,
         }
     }
 }
 impl From<u64> for Abstraction {
     fn from(n: u64) -> Self {
-        match n >> 52 {
-            Self::EQUITY_TAG => Self::Percent(((n >> 44) & 0xFF) as u8),
-            Self::POCKET_TAG => Self::Preflop(Self::mask(n)),
-            _ => Self::Learned(n),
+        match (n >> 56) as u8 {
+            0 => Abstraction::Preflop(n),
+            1 => Abstraction::Learned(n),
+            2 => Abstraction::Learned(n),
+            3 => Abstraction::Percent(n),
+            _ => unreachable!("Invalid street value"),
         }
     }
 }
@@ -98,72 +131,38 @@ impl From<Abstraction> for i64 {
 }
 impl From<i64> for Abstraction {
     fn from(n: i64) -> Self {
-        Self::Learned(n as u64)
+        Self::from(n as u64)
     }
 }
 
-/// lossless preflop abstraction
-impl From<Observation> for Abstraction {
-    fn from(observation: Observation) -> Self {
-        assert!(observation.street() == crate::cards::street::Street::Pref);
-        Self::Preflop(Self::hash(u64::from(Hand::from(Hole::from(observation)))))
-    }
-}
-
-/// probability isomorphism
-///
-/// for river, we use a u8 to represent the equity bucket,
-/// i.e. Equity(0) is the 0% equity bucket,
-/// and Equity(N) is the 100% equity bucket.
-impl From<Probability> for Abstraction {
-    fn from(p: Probability) -> Self {
-        assert!(p >= 0.);
-        assert!(p <= 1.);
-        Self::Percent(Self::quantize(p))
-    }
-}
-impl From<Abstraction> for Probability {
-    fn from(abstraction: Abstraction) -> Self {
-        match abstraction {
-            Abstraction::Percent(n) => Abstraction::floatize(n),
-            Abstraction::Learned(_) => unreachable!("no cluster into probability"),
-            Abstraction::Preflop(_) => unreachable!("no preflop into probability"),
-        }
-    }
-}
-
+/// string isomorophism
 impl TryFrom<&str> for Abstraction {
     type Error = Box<dyn std::error::Error>;
     fn try_from(s: &str) -> Result<Self, Self::Error> {
-        let s = s.trim();
+        let s = s.trim().to_lowercase();
         let n = &s[8..s.len() - 1];
         if false {
             unreachable!()
-        } else if s.starts_with("Percent(") && s.ends_with(")") {
+        } else if s.starts_with("percent(") && s.ends_with(")") {
             Ok(Self::from(Probability::from(n.parse::<Probability>()?)))
-        } else if s.starts_with("Preflop(") && s.ends_with(")") {
+        } else if s.starts_with("preflop(") && s.ends_with(")") {
             Ok(Self::Preflop(u64::from_str_radix(n, 16)?))
-        } else if s.starts_with("Learned(") && s.ends_with(")") {
+        } else if s.starts_with("learned(") && s.ends_with(")") {
             Ok(Self::Learned(u64::from_str_radix(n, 16)?))
         } else {
             Err("invalid Abstraction string".into())
         }
     }
 }
+impl std::fmt::Display for Abstraction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {:02x}", self.street(), self.index())
+    }
+}
 
 impl crate::Arbitrary for Abstraction {
     fn random() -> Self {
         Self::random()
-    }
-}
-
-impl std::fmt::Display for Abstraction {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Learned(n) => write!(f, "Learned({:016x})", n),
-            Self::Preflop(h) => write!(f, "Preflop({:016x})", h),
-            Self::Percent(n) => write!(f, "Percent({:00.2})", Self::floatize(*n)),
-        }
     }
 }
 
@@ -198,12 +197,7 @@ mod tests {
     }
     #[test]
     fn bijective_u64_equity() {
-        let equity = Abstraction::Percent(Abstraction::N / 2);
+        let equity = Abstraction::from(Observation::from(Street::Rive).equity());
         assert_eq!(equity, Abstraction::from(u64::from(equity)));
-    }
-    #[test]
-    fn bijective_u64_pocket() {
-        let pocket = Abstraction::from(Observation::from(Street::Pref));
-        assert_eq!(pocket, Abstraction::from(u64::from(pocket)));
     }
 }
