c8289a7568812efcd6e1d49e5507e63a71b7ff69
Kelechi Ukah <ukahkelechi@gmail.com>
2025-01-23 16:49:23 -0500

    fix street n_isomorphism constants

--- src/cards/isomorphisms.rs
+++ src/cards/isomorphisms.rs
@@ -27,3 +27,40 @@ impl From<Street> for IsomorphismIterator {
         Self(ObservationIterator::from(street))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn n_pref() {
+        let pref = Street::Pref;
+        let iter = IsomorphismIterator::from(pref);
+        assert_eq!(iter.count(), pref.n_isomorphisms());
+    }
+
+    #[test]
+    #[ignore]
+    fn n_flop() {
+        let flop = Street::Flop;
+        let iter = IsomorphismIterator::from(flop);
+        assert_eq!(iter.count(), flop.n_isomorphisms());
+    }
+
+    #[test]
+    #[ignore]
+    fn n_turn() {
+        let turn = Street::Turn;
+        let iter = IsomorphismIterator::from(turn);
+        assert_eq!(iter.count(), turn.n_isomorphisms());
+    }
+
+    #[test]
+    #[ignore]
+    fn n_rive() {
+        let rive = Street::Rive;
+        let iter = IsomorphismIterator::from(rive);
+        assert_eq!(iter.count(), rive.n_isomorphisms());
+    }
+}
