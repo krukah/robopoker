587a10e4fb7d7f30c339ce6b75e8d4e2d1749d1f
Kelechi Ukah <ukahkelechi@gmail.com>
2025-02-20 22:43:28 -0500

    From<Vec<Row>> for Policy

--- src/mccfr/path.rs
+++ src/mccfr/path.rs
@@ -50,6 +50,16 @@ impl From<Path> for u64 {
         path.0
     }
 }
+impl From<Path> for i64 {
+    fn from(path: Path) -> Self {
+        path.0 as i64
+    }
+}
+impl From<i64> for Path {
+    fn from(value: i64) -> Self {
+        Self(value as u64)
+    }
+}
 
 impl std::fmt::Display for Path {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
