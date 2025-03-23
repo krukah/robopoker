9646216b8806b93cccceb0f3731179fe7098c5c3
Drazcmd <Drazcmd@gmail.com>
2025-03-23 12:08:13 -0700

    Add+use TranslatedAction struct return type
    I think this is basically the MVP for psuedoharmonic action translation here. Going to probably add a couple more test cases and then wrap things up here.
    
    (Side note: still need to double check whether the annotations on the struct are correct/expected; cursory search hasn't shown me a definitive answer yet on the best way to structure things when creating a struct just to serve as the return type like this in Rust)

--- src/translate_action/translate_action.rs
+++ src/translate_action/translate_action.rs
@@ -1,3 +1,20 @@
+///   contains less than two actions, or contains any actions less than 1 chip.
+use crate::Chips;
+
+/// Translated Action sizes and percentages to use each one.
+///
+/// * Percentages should always sum to 1.0.
+/// * When returning for only one size, smaller_size and larger_size should be set to the same
+///   value, and smaller_percentage should bet set to 1.0 and larger_size should bet set to 0.0.
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct TranslatedAction {
+    smaller_size: Chips,
+    larger_size: Chips,
+    smaller_percentage: f64,
+    larger_percentage: f64,
+}
+
 /// Translates an incoming bet size to a randomized choice of a smaller and larger bet size.
 ///
 /// # Arguments TODO
@@ -12,10 +29,11 @@
 /// * When pot_size is less than 1 chip
 /// * When opponent_bet is less than 1 chip
 /// * When action_abstraction isn't sorted in ascending order, doesn't contain only unique values,
-///   contains less than two actions, or contains any actions less than 1 chip.
-use crate::Chips;
-
-pub fn translate_action(pot_size: Chips, opponent_bet: Chips, action_abstraction: &[Chips]) -> f64 {
+pub fn translate_action(
+    pot_size: Chips,
+    opponent_bet: Chips,
+    action_abstraction: &[Chips],
+) -> TranslatedAction {
     if pot_size <= 1 || opponent_bet < 1 {
         panic!("pot_size and opponent_bet must both be at least 1 chip.")
     }
@@ -38,26 +56,40 @@ pub fn translate_action(pot_size: Chips, opponent_bet: Chips, action_abstraction
 
     // If opponent_bet is itself an option in action_abstraction then no need to randomize!
     if action_abstraction.contains(&opponent_bet) {
-        todo!("Early return: use opponent_bet at 100% frequency.")
+        return TranslatedAction {
+            smaller_size: opponent_bet,
+            larger_size: opponent_bet,
+            smaller_percentage: 1.0,
+            larger_percentage: 0.0,
+        };
     }
 
     // If opponent_bet is outside the range of bets in the action_abstraction then there's no
     // point in randomizing. As in the paper, all we can do in such cases is to simply use
     // the closet bet (i.e. smallest or largest size) 100% of the time.
     if opponent_bet < action_abstraction[0] {
-        todo!("Early return: (sadly) use smallest bet in abstraction at 100% frequency.")
+        return TranslatedAction {
+            smaller_size: action_abstraction[0],
+            larger_size: action_abstraction[0],
+            smaller_percentage: 1.0,
+            larger_percentage: 0.0,
+        };
     }
-    if opponent_bet
-        > action_abstraction
-            .last()
-            .copied()
-            .expect("Should never see this; we verified above that it >= 2 elements.")
-    {
-        todo!("Early return: (sadly) use largest bet in abstraction at 100% frequency ")
+    let largest_abstraction_size = action_abstraction
+        .last()
+        .copied()
+        .expect("Should never see this; we verified above that it has >= 2 elements.");
+    if opponent_bet > largest_abstraction_size {
+        return TranslatedAction {
+            smaller_size: largest_abstraction_size,
+            larger_size: largest_abstraction_size,
+            smaller_percentage: 1.0,
+            larger_percentage: 0.0,
+        };
     }
 
     // Now we've finally verified the inputs are good and that we cannot short-cirtcuit. Therefore:
-    // we can, an need to, actually choose two actions to translate opponent_bet to, as well as
+    // we can, an need to, actually choose two actions to translate opponent_bet to. As well as
     // calculate the percentages we should randomize between them.
 
     let actions_to_randomize_between = action_abstraction
@@ -75,10 +107,14 @@ pub fn translate_action(pot_size: Chips, opponent_bet: Chips, action_abstraction
         actions_to_randomize_between[1] as f64 / pot_size_f64,
         opponent_bet as f64 / pot_size_f64,
     );
-    let _probability_larger_bet = 1.0 - probabilty_smaller_bet;
 
-    todo!("Return frequency and original Chips size of BOTH actions we found here.");
-    probabilty_smaller_bet
+    TranslatedAction {
+        smaller_size: actions_to_randomize_between[0],
+        larger_size: actions_to_randomize_between[1],
+
+        smaller_percentage: probabilty_smaller_bet,
+        larger_percentage: 1.0 - probabilty_smaller_bet,
+    }
 }
 
 /// Calculates psuedo-harmonic mapping percentage to use the smaller bet.
@@ -110,19 +146,30 @@ mod tests {
 
     #[test]
     fn test_size_in_abstraction() {
-        translate_action(1000, 6, [2, 6, 9].as_slice());
-        todo!("Add test case once we figure out return type.")
+        let translation = translate_action(1000, 6, [2, 6, 9].as_slice());
+
+        assert_eq!(translation.smaller_size, 6);
+        assert_eq!(translation.larger_size, 6);
+        assert!((translation.smaller_percentage - 1.0).abs() < f64::EPSILON);
+        assert!(translation.larger_percentage.abs() < f64::EPSILON);
     }
 
     #[test]
     fn test_size_smaller_than_any_in_abstraction() {
-        translate_action(1000, 8, [10, 20, 30].as_slice());
-        todo!("Add test case once we figure out return type.")
+        let translation = translate_action(1000, 8, [10, 20, 30].as_slice());
+
+        assert_eq!(translation.smaller_size, 10);
+        assert_eq!(translation.larger_size, 10);
+        assert!((translation.smaller_percentage - 1.0).abs() < f64::EPSILON);
+        assert!(translation.larger_percentage.abs() < f64::EPSILON);
     }
     #[test]
     fn test_size_larger_than_any_in_abstraction() {
-        translate_action(1000, 37, [10, 20, 30].as_slice());
-        todo!("Add test case once we figure out return type.")
+        let translation = translate_action(1000, 37, [10, 20, 30].as_slice());
+        assert_eq!(translation.smaller_size, 30);
+        assert_eq!(translation.larger_size, 30);
+        assert!((translation.smaller_percentage - 1.0).abs() < f64::EPSILON);
+        assert!(translation.larger_percentage.abs() < f64::EPSILON);
     }
 
     #[test]
