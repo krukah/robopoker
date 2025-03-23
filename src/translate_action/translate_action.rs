655e431dfe00ababa4298d679c9998de1231fab9
Drazcmd <Drazcmd@gmail.com>
2025-03-23 15:57:34 -0700

    Final testing + comment/docstring cleanup
    Should be about ready to send pull request now!

--- src/translate_action/translate_action.rs
+++ src/translate_action/translate_action.rs
@@ -1,11 +1,6 @@
-///   contains less than two actions, or contains any actions less than 1 chip.
 use crate::Chips;
 
-/// Translated Action sizes and percentages to use each one.
-///
-/// * Percentages should always sum to 1.0.
-/// * When returning for only one size, smaller_size and larger_size should be set to the same
-///   value, and smaller_percentage should bet set to 1.0 and larger_size should bet set to 0.0.
+/// Provides the return type for translate_action() - see below for more details.
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct TranslatedAction {
@@ -17,25 +12,28 @@ pub struct TranslatedAction {
 
 /// Translates an incoming bet size to a randomized choice of a smaller and larger bet size.
 ///
-/// # Arguments TODO
+/// # Arguments
 /// * `pot_size` - The current size of the pot
 /// * `opponent_bet` - The actual bet made by the opponent
 /// * `action_abstraction` - The list of allowed sizes to translate to.
 ///
 /// # Returns
-/// * TODO create a struct return type
+/// * Translated Action - contains action sizes and the percentage to use both respective size (out
+///   of 1.0). When returning for only one size, smaller_size and larger_size will be set to the
+///   same value, smaller_percentage will be set to 1.0, and larger_size will bet set to 0.0.
 ///
 /// # Panics
 /// * When pot_size is less than 1 chip
 /// * When opponent_bet is less than 1 chip
 /// * When action_abstraction isn't sorted in ascending order, doesn't contain only unique values,
+///   contains less than two actions, or contains any actions less than 1 chip.
 pub fn translate_action(
     pot_size: Chips,
     opponent_bet: Chips,
     action_abstraction: &[Chips],
 ) -> TranslatedAction {
-    if pot_size <= 1 || opponent_bet < 1 {
-        panic!("pot_size and opponent_bet must both be at least 1 chip.")
+    if pot_size <= 1 || opponent_bet < 1 || action_abstraction.into_iter().any(|&size| size < 1) {
+        panic!("pot_size, opponent_bet, and all action_abstraction sizes must be at least 1 chip.")
     }
     if action_abstraction.len() < 2 {
         panic!("action_abstraction must have at least 2 elements.")
@@ -88,9 +86,9 @@ pub fn translate_action(
         };
     }
 
-    // Now we've finally verified the inputs are good and that we cannot short-cirtcuit. Therefore:
-    // we can, an need to, actually choose two actions to translate opponent_bet to. As well as
-    // calculate the percentages we should randomize between them.
+    // Now we've finally verified the inputs are good and that we cannot early return. Therefore:
+    // we can, and need to, choose two actions to translate opponent_bet to (as well as calculate
+    // the percentages we should randomize between them).
 
     let actions_to_randomize_between = action_abstraction
         .windows(2)
@@ -99,10 +97,9 @@ pub fn translate_action(
 
     let pot_size_f64 = pot_size as f64;
     let probabilty_smaller_bet = calc_pseudo_harmonic_mapping(
-        // Scale down everything by pot to ensure we provide scale
-        // invariance. (This is not explained clearly in the paper, but
-        // I believe it's actually _why_ they scaled everything down so that it
-        // was relative to a pot size of 1
+        // Scale down everything by pot to ensure we provide scale invariance. (This is not
+        // explained super clearly in the paper, but I believe it's *why* they scaled everything
+        // down to be relative to a pot size of 1 when they were actually analyzing things.)
         actions_to_randomize_between[0] as f64 / pot_size_f64,
         actions_to_randomize_between[1] as f64 / pot_size_f64,
         opponent_bet as f64 / pot_size_f64,
@@ -122,7 +119,7 @@ pub fn translate_action(
 /// NOTE: INPUTS MUST ALL BE NORMALIZED RELATIVE TO THE POT SIZE (ie pot = 1.0).
 /// (Hence the '_ratio' in each name.)
 ///
-/// Formula: f_A,B (x) = ((B - x) * (1 + A) / ((B - A) * (1 + x))
+/// Formula: f_A,B (x) = ((B - x) * (1 + A)) / ((B - A) * (1 + x))
 /// Where: A=smaller_bet/pot, B=larger_bet/pot, x=opponent_bet/pot
 fn calc_pseudo_harmonic_mapping(
     smaller_bet_ratio: f64,
@@ -143,6 +140,40 @@ fn calc_pseudo_harmonic_mapping(
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_happy_path_calculation() {
+        // Arbitrarily choosing 5 decimal places.
+        let precision = 0.00001;
+
+        let translation = translate_action(100, 50, [11, 25, 75, 133, 30101].as_slice());
+
+        assert_eq!(translation.smaller_size, 25);
+        assert_eq!(translation.larger_size, 75);
+
+        // When A=0.25, B=0.75, x=0.5,
+        // f(A, B, x) = ((B - x) * (1 + A)) / ((B - A) * (1 + x))
+        //            = 0.25 * 1.25 / (.5 * 1.5)
+        //            = 0.3125 / 0.75
+        //            = (5/16) * (4/3)
+        //            = 5/12
+        //            = 0.4166666666...
+        assert!((translation.smaller_percentage - 0.41667).abs() < precision);
+        assert!((translation.larger_percentage - 0.58333).abs() < precision);
+    }
+
+    #[test]
+    fn test_no_max_i32_overflow() {
+        // Arbitrarily choosing to go out to 5 decimal places.
+        let precision = 0.00001;
+
+        let translation = translate_action(1000, 500, [1, 250, 750, i16::MAX].as_slice());
+
+        assert_eq!(translation.smaller_size, 250);
+        assert_eq!(translation.larger_size, 750);
+        assert!((translation.smaller_percentage - 0.41667).abs() < precision);
+        assert!((translation.larger_percentage - 0.58333).abs() < precision);
+    }
 
     #[test]
     fn test_size_in_abstraction() {
@@ -166,10 +197,47 @@ mod tests {
     #[test]
     fn test_size_larger_than_any_in_abstraction() {
         let translation = translate_action(1000, 37, [10, 20, 30].as_slice());
+
         assert_eq!(translation.smaller_size, 30);
         assert_eq!(translation.larger_size, 30);
         assert!((translation.smaller_percentage - 1.0).abs() < f64::EPSILON);
         assert!(translation.larger_percentage.abs() < f64::EPSILON);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_disallows_zero_opponent_bet_size() {
+        translate_action(100, 0, [5, 6].as_slice());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_disallows_negative_opponent_bet_size() {
+        translate_action(100, -4, [5, 6].as_slice());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_disallows_zero_pot_size() {
+        translate_action(0, 1, [5, 6].as_slice());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_disallows_negative_pot_size() {
+        translate_action(-4, 1, [5, 6].as_slice());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_disallows_zero_chip_action_abstraction() {
+        translate_action(1000, 1, [0, 4, 6].as_slice());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_disallows_negative_chip_action_abstraction() {
+        translate_action(1000, 1, [-4, 4, 6].as_slice());
     }
 
     #[test]
@@ -184,10 +252,9 @@ mod tests {
         translate_action(1000, 3, [2, 5, 4, 6].as_slice());
     }
 
-    // See "Table 1: Effect of increasing A while holding B = 1 and x = 0.25 fixed."
-    // in the paper.
+    // See "Table 1: Effect of increasing A while holding B = 1 and x = 0.25 fixed".
     //
-    // NOTE: Tests the internal calc_pseudo_harmonic_mapping, not translate_action.
+    // NOTE: Tests the internal calc_pseudo_harmonic_mapping(), not the public translate_action().
     #[test]
     fn test_replicates_paper_table_1_results() {
         let b = 1.0;
