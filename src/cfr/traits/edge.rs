7a96afb2c02bca17d8188186ce77338cf7941b44
Kelechi Ukah <ukahkelechi@gmail.com>
2025-05-04 00:18:33 -0400

    policy alternative exploration

--- src/cfr/traits/edge.rs
+++ src/cfr/traits/edge.rs
@@ -1,5 +1,15 @@
 /// the edge is fully abstracted. it is basically a marker trait
 pub trait Edge:
-    Copy + Clone + PartialEq + Eq + Send + Sync + crate::transport::support::Support + std::fmt::Debug
+    Copy
+    + Clone
+    + PartialEq
+    + Eq
+    + PartialOrd // can be ignored
+    + Ord // can be ignored
+    + Send
+    + Sync
+    + crate::transport::support::Support
+    + std::hash::Hash // can be ignored
+    + std::fmt::Debug
 {
 }
--- src/cfr/types/policy.rs
+++ src/cfr/types/policy.rs
@@ -6,6 +6,13 @@
 /// BTreeMap or HashMap, which yields better asymptotic
 /// performance with O(1) lookups. but i feel like in
 /// practice, iterating over the vector until we find
-/// equality might be more efficient. vibes-based
-/// analysis to be clear, not at all benchmarked yet.
+/// equality might be more efficient.
+///
+/// benchmarks from RPS (which has a fixed number 3 of
+/// actions) show that this is more efficient than
+/// the other two implementations. i'm going to leave this as a Vec for now because
+/// it has the lowest overhead.
+
 pub type Policy<E> = Vec<(E, crate::Probability)>;
+// pub type Policy<E> = std::collections::HashMap<E, crate::Probability>;
+// pub type Policy<E> = std::collections::BTreeMap<E, crate::Probability>;
