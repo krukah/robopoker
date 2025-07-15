bccd7fa88ab46cb4a1953d726ef3a932382c947c
CD <Drazcmd@users.noreply.github.com>
2025-07-14 23:54:19 -0700

    Yep definitely no more frequently with this!
    Took like 20 runs before erroring at 500 iterations per. So almost certainly MUST be happening onthe first one

--- src/clustering/kmeans.rs
+++ src/clustering/kmeans.rs
@@ -985,11 +985,8 @@ mod tests {
             init_centers,
             points: &points,
             kmeans_k: 2,
-            // TODO: Don't leave at 1, bump this back up and check RMS
-            // actually decreases. (This is just to demonstrate that
-            // the current flakey error almost certainly only occurs on
-            // the very first iteration...)
-            iterations_t: 1,
+            // TODO lower back down
+            iterations_t: 500,
             label: "test_elkan".to_string(),
         };
 
