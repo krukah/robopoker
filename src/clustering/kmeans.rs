87d129c785ad9aa68107673883100ea700e289eb
CD <Drazcmd@users.noreply.github.com>
2025-07-14 21:50:10 -0700

    NEVERMIND - error isn't what I thoguht it was!
    ```
    thread 'clustering::kmeans::tests::test_kmeans_elkan_rms_decreases' panicked at src/clustering/kmeans.rs:574:49:
    index out of bounds: the len is 0 but the index is 0
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    ```
    nothing to do with the RMS decreasing here! (# iterations didn't do anything)

--- src/clustering/kmeans.rs
+++ src/clustering/kmeans.rs
@@ -903,31 +903,11 @@ mod tests {
                 create_equity_abstraction(0.88),
                 create_equity_abstraction(0.87),
             ]),
-            create_test_histogram(vec![
-                create_equity_abstraction(0.111),
-                create_equity_abstraction(0.104),
-                create_equity_abstraction(0.145),
-            ]),
-            create_test_histogram(vec![
-                create_equity_abstraction(0.2),
-                create_equity_abstraction(0.084),
-                create_equity_abstraction(0.1322),
-            ]),
-            create_test_histogram(vec![
-                create_equity_abstraction(0.444),
-                create_equity_abstraction(0.859),
-                create_equity_abstraction(0.984),
-            ]),
-            create_test_histogram(vec![
-                create_equity_abstraction(0.822),
-                create_equity_abstraction(0.881),
-                create_equity_abstraction(0.873),
-            ]),
         ];
 
         let init_centers = vec![
-            create_test_histogram(vec![create_equity_abstraction(0.01)]),
-            create_test_histogram(vec![create_equity_abstraction(0.99)]),
+            create_test_histogram(vec![create_equity_abstraction(0.2)]),
+            create_test_histogram(vec![create_equity_abstraction(0.7)]),
         ];
 
         let clusterable = MockClusterable {};
@@ -936,7 +916,7 @@ mod tests {
             init_centers,
             points: &points,
             kmeans_k: 2,
-            iterations_t: 200,
+            iterations_t: 50,
             label: "test_elkan".to_string(),
         };
 
